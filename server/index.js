9a918cb55346494d286e2959ebc4e6125e4e1561
jacob knudsen <jhakeknudsen@protonmail.com>
2022-03-04 14:30:58 -0700

    final commit

--- server/index.js
+++ server/index.js
@@ -21,5 +21,33 @@ app.get("/api/compliment", (req, res) => {
   res.status(200).send(randomCompliment);
   
 });
+app.get("/api/ranfortune", (req, res) => {
+  const fortune = ["dang you look good today!",
+					 "is that bathing ape?! its in your furture",
+					 "your going to recive 1 million dollars.",
+           "you'll go to the league!",
+           "all your hard work is going to pay off!"
+  ];
+
+  let randomIndex = Math.floor(Math.random() * fortune.length);
+  let randomfortune = fortune[randomIndex];
+
+  res.status(200).send(randomfortune);
+  
+});
+app.get("/api/weather", (req, res) => {
+  const fortune = ["dang its hot!",
+					 "its so cold",
+					 "i love the rain",
+           "pls we need more snow!",
+           "its a perfect beach day!"
+  ];
+
+  let randomIndex = Math.floor(Math.random() * fortune.length);
+  let randomfortune = fortune[randomIndex];
+
+  res.status(200).send(randomfortune);
+  
+});
 
 app.listen(4000, () => console.log("Server running on 4000"));
